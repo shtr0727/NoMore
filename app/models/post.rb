88bb6663002eb0e7945a93bfd234352577f88dcd
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-07-03 22:27:08 +0900

    [add]

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :streak, optional: true
-  has_many :favorites
-  has_many :comments
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
