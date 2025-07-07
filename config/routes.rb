c65c9cd716c149a0da9d7e282347013f2cc32e1b
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-07-07 23:50:49 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tops#top"
+  get "home", to: "homes#index"
 
   resources :not_todos
 end
