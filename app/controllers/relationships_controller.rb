f9bd27581041049bc536f7357e7f01c56789c899
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-09-03 23:50:56 +0900

    fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find(params[:followed_id])
+    user = User.find(params[:relationship][:followed_id])
     current_user.follow(user)
     redirect_to user
   end
