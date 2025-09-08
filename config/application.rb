6ee238e7348fb974c001aefb5cd8bb3483f67343
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-09-08 23:55:43 +0900

    fix 年月日表記

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module NoMore
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # Set default locale to Japanese
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
   end
 end
