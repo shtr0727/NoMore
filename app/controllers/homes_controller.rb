6e82ccdf9f1d192dddad4a4045490c169a1e9c66
shtr0727 <193111456+shtr0727@users.noreply.github.com>
2025-09-15 01:36:17 +0900

    fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @posts = Post.where(is_draft: false).includes(:streak, :user, :category).order(created_at: :desc).page(params[:page]).per(6)
+    @posts = Post.where(is_draft: false).includes(:streak, :user, :category).order(recorded_on: :desc, created_at: :desc).page(params[:page]).per(6)
   end
 end
